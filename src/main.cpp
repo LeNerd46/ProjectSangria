ab5f9c319ff294d00700bade9665b1f990e31c77
Devon Shoutz <devon.shoutz@gmail.com>
2024-01-28 22:07:15 -0600

    I'm sorry

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 
 #include "utils.h"
 
+/*
+* This is a very personal project, and I just want to share my story
+* This story has changed throughout the past few years, and I'm still finding the best way to tell it
+* I just want to say, I'm sorry. I know you won't see this, but I want to say it anyway
+* I should've been more caring, I should've been there for you, I should've been better
+* I know it's too late now, what's done is done, but I just really want you to know that I do still care
+* You might hate me, that's fine, I probably would too were I in your shoes
+* I'm still so grateful for the time I got to spend with you, thank you for all the memories
+* I never got to do it then, so I'm doing it now
+* This is my way of saying sorry, my way of saying goodbye
+* Even if we're two different people now, even if we'll never see each other again, I just want to say
+* 
+* I still care about you, and I'm sorry
+* Thank you... for our time together
+*/
+
 void titleScreen()
 {
 	do
